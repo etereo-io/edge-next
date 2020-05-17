df0303d5e58b62e91f9998024a1e7e8818f0bbd6
Hayder Al-Deen <hayder@etereo.io>
2020-05-17 13:38:30 +0200

    left column

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 import Layout from '../components/layout/normal/layout'
+import Avatar from '../components/user/avatar/avatar'
+import { useRouter } from 'next/router'
+import useSWR from 'swr'
 import Button from '../components/generic/button/button'
 import GithubLogo from '../components/generic/icons/github-icon/github-icon'
 import NextJSLogo from '../components/generic/icons/nextjs-icon/nextjs-icon'
@@ -10,6 +13,13 @@ import ContentListView from '../components/content/read-content/content-list-vie
 
 const Landing = (props) => {
   const { contentType } = useContentType('post')
+
+  // Load profile data
+  const router = useRouter()
+  const { userId } = router.query
+  const { data, error } = useSWR( userId ? `/api/users/` + userId : null, fetch)
+  const finished = Boolean(data) || Boolean(error)
+
   return (
     <>
       <Layout
@@ -17,6 +27,13 @@ const Landing = (props) => {
         description={config.slogan}
       >
         <div className="columns">
+          <div className="left-column">
+            <div className="avatar">
+              <Avatar src={data ? data.profile.picture : null} />
+            </div>
+            <strong>Hayder Al-Deen</strong>
+            <span>@hayder</span>
+          </div>
           <div className="center-column">
             {contentType && <ContentListView type={contentType} />}
           </div>
@@ -30,33 +47,26 @@ const Landing = (props) => {
                   viewBox="0 0 32 32"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
-                 
                 >
                   <path
                     d="M9.33331 0H4C1.79031 0 0 1.79031 0 4V9.33331C0 11.543 1.79031 13.3333 4 13.3333H9.33331C11.543 13.3333 13.3333 11.543 13.3333 9.33331V4C13.3333 1.79031 11.5431 0 9.33331 0ZM10.6667 9.33331C10.6667 10.0702 10.0703 10.6666 9.33337 10.6666H4C3.26306 10.6666 2.66669 10.0702 2.66669 9.33331V4C2.66669 3.26306 3.26306 2.66669 4 2.66669H9.33331C10.0702 2.66669 10.6666 3.26306 10.6666 4V9.33331H10.6667Z"
                     fill="var(--empz-success)"
-                   
                   ></path>
                   <path
                     d="M28.0001 0H22.6667C20.4571 0 18.6667 1.79031 18.6667 4V9.33331C18.6667 11.543 20.4571 13.3333 22.6667 13.3333H28.0001C30.2097 13.3333 32.0001 11.543 32.0001 9.33331V4C32.0001 1.79031 30.2097 0 28.0001 0ZM29.3334 9.33331C29.3334 10.0702 28.737 10.6666 28.0001 10.6666H22.6667C21.9298 10.6666 21.3334 10.0702 21.3334 9.33331V4C21.3334 3.26306 21.9298 2.66669 22.6667 2.66669H28.0001C28.737 2.66669 29.3334 3.26306 29.3334 4V9.33331Z"
                     fill="var(--empz-success)"
-                   
                   ></path>
                   <path
                     d="M9.33331 18.6667H4C1.79031 18.6667 0 20.4571 0 22.6667V28.0001C0 30.2097 1.79031 32.0001 4 32.0001H9.33331C11.543 32.0001 13.3333 30.2097 13.3333 28.0001V22.6667C13.3333 20.457 11.5431 18.6667 9.33331 18.6667ZM10.6667 28.0001C10.6667 28.737 10.0703 29.3334 9.33337 29.3334H4C3.26306 29.3334 2.66669 28.737 2.66669 28.0001V22.6667C2.66669 21.9298 3.26306 21.3334 4 21.3334H9.33331C10.0702 21.3334 10.6666 21.9298 10.6666 22.6667V28.0001H10.6667Z"
                     fill="var(--empz-success)"
-                   
                   ></path>
                   <path
                     d="M30.6667 24.0001H26.6667V20.0001C26.6667 19.2637 26.0698 18.6667 25.3334 18.6667C24.597 18.6667 24.0001 19.2637 24.0001 20.0001V24.0001H20.0001C19.2637 24.0001 18.6667 24.597 18.6667 25.3334C18.6667 26.0697 19.2637 26.6667 20.0001 26.6667H24.0001V30.6667C24.0001 31.4031 24.597 32 25.3334 32C26.0697 32 26.6667 31.4031 26.6667 30.6667V26.6667H30.6667C31.4031 26.6667 32 26.0697 32 25.3334C32.0001 24.597 31.4031 24.0001 30.6667 24.0001Z"
                     fill="var(--empz-success)"
-                   
                   ></path>
                 </svg>
                 <h6>Components</h6>
-                <p>
-                  Complete set of ready to use web components.
-                </p>
+                <p>Complete set of ready to use web components.</p>
               </a>
             </div>
             <div className="call-to-action-item alt">
@@ -67,18 +77,14 @@ const Landing = (props) => {
                   viewBox="0 0 32 32"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
-                 
                 >
                   <path
                     d="M24.3158 0.475188C24.1491 0.18125 23.8378 0 23.5 0H12.25C11.8197 0 11.4452 0.293 11.3409 0.710437L7.59087 15.7729C7.52037 16.0531 7.58356 16.3498 7.76119 16.5767C7.93881 16.8047 8.21163 16.9374 8.5 16.9374H12.7719L7.62206 30.7329C7.45725 31.1723 7.64312 31.6658 8.056 31.8883C8.47075 32.1105 8.98388 31.9916 9.25806 31.6136L24.2581 10.9261C24.4659 10.6413 24.4952 10.2641 24.3359 9.94919C24.1757 9.63519 23.8525 9.43744 23.5 9.43744H19.5312L24.3038 1.41994C24.4778 1.12975 24.4824 0.769063 24.3158 0.475188Z"
                     fill="var(--empz-background)"
-                   
                   ></path>
                 </svg>
                 <h6>Fast Rendering</h6>
-                <p>
-                  Dynamic content that loads at lightning speed.
-                </p>
+                <p>Dynamic content that loads at lightning speed.</p>
               </a>
             </div>
             <div className="call-to-action-item featured">
@@ -88,18 +94,15 @@ const Landing = (props) => {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 40 38"
-                 
                 >
                   <g fill="var(--empz-success)">
-                    <path
-                      d="M13.7551 26.0833c0-1.1274-.3438-2.0559-1.0314-2.7854-.6875-.7428-1.8334-1.4059-3.4377-1.9896-1.6043-.5969-2.8521-1.1472-3.7434-1.6513-2.9667-1.658-4.45-4.1118-4.45-7.3614 0-2.2018.643-4.0123 1.929-5.4316 1.286-1.4192 2.6483-2.2217 4.851-2.487V0h3.4379v4.377c2.2154.3316 3.9279 1.3132 5.1375 2.9446 1.2097 1.6182 1.8144 3.7272 1.8144 6.3269h-4.6219c0-1.6712-.3629-2.9844-1.0887-3.9394-.7129-.9682-1.687-1.4524-2.922-1.4524-1.2224 0-2.1773.3449-2.8649 1.0346-.6875.6897-1.0313 1.6779-1.0313 2.9645 0 1.1539.3374 2.0824 1.0122 2.7854.6876.6897 1.8462 1.3463 3.476 1.9697 1.6298.6233 2.9094 1.2003 3.8389 1.7309.9295.5305 1.7125 1.1407 2.3491 1.8304.6367.6764 1.1269 1.459 1.4706 2.3477.3438.8887.5157 1.9298.5157 3.1237 0 2.2415-.6621 4.0587-1.9863 5.4514-1.3114 1.3926-2.903 2.2349-5.2585 2.4869v3.0573H7.8726v-3.0573c-2.521-.2918-4.4627-1.2335-5.825-2.8252C.6978 29.5651.023 27.4561.023 24.8298h4.641c0 1.6714.4011 2.9646 1.2033 3.8798.8149.9152 1.9608 1.3728 3.4377 1.3728 1.4515 0 2.5529-.3648 3.3042-1.0943.7639-.7295 1.1459-1.6978 1.1459-2.9048zM27.5818 15.433h-3.0622v9.2598h3.0622V15.433zM33.706 12.3464h-3.0621v15.4331h3.0621V12.3464zM39.8304 9.2598h-3.0622V30.866h3.0622V9.2598z"
-                     
-                    ></path>
+                    <path d="M13.7551 26.0833c0-1.1274-.3438-2.0559-1.0314-2.7854-.6875-.7428-1.8334-1.4059-3.4377-1.9896-1.6043-.5969-2.8521-1.1472-3.7434-1.6513-2.9667-1.658-4.45-4.1118-4.45-7.3614 0-2.2018.643-4.0123 1.929-5.4316 1.286-1.4192 2.6483-2.2217 4.851-2.487V0h3.4379v4.377c2.2154.3316 3.9279 1.3132 5.1375 2.9446 1.2097 1.6182 1.8144 3.7272 1.8144 6.3269h-4.6219c0-1.6712-.3629-2.9844-1.0887-3.9394-.7129-.9682-1.687-1.4524-2.922-1.4524-1.2224 0-2.1773.3449-2.8649 1.0346-.6875.6897-1.0313 1.6779-1.0313 2.9645 0 1.1539.3374 2.0824 1.0122 2.7854.6876.6897 1.8462 1.3463 3.476 1.9697 1.6298.6233 2.9094 1.2003 3.8389 1.7309.9295.5305 1.7125 1.1407 2.3491 1.8304.6367.6764 1.1269 1.459 1.4706 2.3477.3438.8887.5157 1.9298.5157 3.1237 0 2.2415-.6621 4.0587-1.9863 5.4514-1.3114 1.3926-2.903 2.2349-5.2585 2.4869v3.0573H7.8726v-3.0573c-2.521-.2918-4.4627-1.2335-5.825-2.8252C.6978 29.5651.023 27.4561.023 24.8298h4.641c0 1.6714.4011 2.9646 1.2033 3.8798.8149.9152 1.9608 1.3728 3.4377 1.3728 1.4515 0 2.5529-.3648 3.3042-1.0943.7639-.7295 1.1459-1.6978 1.1459-2.9048zM27.5818 15.433h-3.0622v9.2598h3.0622V15.433zM33.706 12.3464h-3.0621v15.4331h3.0621V12.3464zM39.8304 9.2598h-3.0622V30.866h3.0622V9.2598z"></path>
                   </g>
                 </svg>
                 <h6>Web Monetization</h6>
                 <p>
-                  Integrate a simple JS API to create payment stream with just a few lines of code.
+                  Integrate a simple JS API to create payment stream with just a
+                  few lines of code.
                 </p>
               </a>
             </div>
@@ -111,7 +114,7 @@ const Landing = (props) => {
                 target="_blank"
               >
                 <span>Powered by</span>
-                <NextJSLogo width="80px"/>
+                <NextJSLogo width="80px" />
               </a>
             </div>
 
@@ -123,7 +126,7 @@ const Landing = (props) => {
                 title="Source code"
               >
                 <span>Clone from</span>
-                <GithubLogo width="40px"/>
+                <GithubLogo width="40px" />
               </a>
             </div>
           </div>
@@ -131,13 +134,28 @@ const Landing = (props) => {
       </Layout>
       <style jsx>{`
         .columns {
+          align-items: flex-start;
           background: var(--accents-1);
           display: flex;
           justify-content: space-between;
         }
 
-        @media all and (max-width: 860px){
+        @media all and (max-width: 860px) {
           flex-flow: column;
+        }
+
+        .left-column{
+          align-items: center;
+          background: var(--empz-background);
+          border-radius: var(--empz-radius);
+          box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
+          color: var(--empz-foreground);
+          display: flex;
+          flex-flow: column;
+          padding: var(--empz-gap);
+          position: sticky;
+          top: 80px;
+          width: 20%;
         }
 
         .right-column {
@@ -150,44 +168,43 @@ const Landing = (props) => {
           width: 28.5%;
         }
 
-        @media all and (max-width: 1080px){
-          .right-column{
+        @media all and (max-width: 1080px) {
+          .right-column {
             min-width: 210px;
             width: 20%;
           }
         }
 
-        @media all and (max-width: 860px){
-          .right-column{
+        @media all and (max-width: 860px) {
+          .right-column {
             padding: var(--empz-gap-double);
             width: 100%;
           }
         }
 
-        @media all and (max-width: 600px){
-          .right-column{
+        @media all and (max-width: 600px) {
+          .right-column {
             padding: var(--empz-gap);
             width: 100%;
           }
         }
 
         .center-column {
-          margin-left: auto;
-          margin-right: var(--empz-gap);
+          margin: 0 var(--empz-gap);
           max-width: 600px;
           width: 77%;
         }
 
-        @media all and (max-width: 860px){
-          .center-column{
+        @media all and (max-width: 860px) {
+          .center-column {
             margin: 0;
             padding: var(--empz-gap-double);
             width: 100%;
           }
         }
 
-        @media all and (max-width: 600px){
-          .center-column{
+        @media all and (max-width: 600px) {
+          .center-column {
             margin: 0;
             padding: var(--empz-gap);
             width: 100%;
@@ -201,7 +218,7 @@ const Landing = (props) => {
 
         .call-to-action-item {
           background: var(--empz-background);
-          box-shadow: 0 2px 4px rgba(0,0,0,0.05);
+          box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
           border-radius: 4px;
           margin-bottom: var(--empz-gap);
           transition: box-shadow 0.25s ease, transform 0.25s ease;
@@ -209,15 +226,15 @@ const Landing = (props) => {
           will-change: box-shadow, transform;
         }
 
-        @media all and (max-width: 1080px){
-          .call-to-action-item{
+        @media all and (max-width: 1080px) {
+          .call-to-action-item {
             width: 100%;
           }
         }
 
-        @media all and (max-width: 860px){
-          .call-to-action-item:hover{
-            transform: scale(1.05)!important;
+        @media all and (max-width: 860px) {
+          .call-to-action-item:hover {
+            transform: scale(1.05) !important;
           }
         }
 
@@ -259,7 +276,8 @@ const Landing = (props) => {
           width: 100%;
         }
 
-        .call-to-action-item.featured h6, .call-to-action-item.featured p{
+        .call-to-action-item.featured h6,
+        .call-to-action-item.featured p {
           color: var(--empz-background);
         }
 
