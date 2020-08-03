481e649779e6e387e25e6c9f20952d67a91c7e81
Ihor Anishchenko <igor.anishenko@kitrum.com>
2020-08-03 17:54:55 +0300

    feat(group creation): added acl for server-side fetching

--- pages/create/group/[type].tsx
+++ pages/create/group/[type].tsx
@@ -1,10 +1,53 @@
 import GroupForm from '@components/groups/write/group-form/group-form'
+import { groupPermission } from '@lib/permissions'
 import Layout from '@components/layout/normal/layout'
 import { usePermission } from '@lib/client/hooks'
 import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
 import LoadingPage from '@components/generic/loading/loading-page/loading-page'
+import { GetServerSideProps } from 'next'
+import { connect } from '@lib/api/db'
 import { getGroupTypeDefinition } from '@lib/config'
+import { getSession } from '@lib/api/auth/iron'
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  res,
+  query,
+}) => {
+  const groupTypeDefinition = getGroupTypeDefinition(query.type)
+
+  // check if group is not in groups mapping
+  if (!groupTypeDefinition) {
+    res.writeHead(302, { Location: '/404' })
+    res.end()
+    return
+  }
+
+  await connect()
+
+  const currentUser = await getSession(req)
+
+  // check if current user can create a group
+  const canAccess = groupPermission(
+    currentUser,
+    groupTypeDefinition.slug,
+    'create'
+  )
+
+  if (!canAccess) {
+    // User dot not have access for creation
+    res.writeHead(302, { Location: '/404' })
+    res.end()
+    return
+  }
+
+  return {
+    props: {
+      groupType: groupTypeDefinition,
+    },
+  }
+}
 
 const CreateGroup = () => {
   const router = useRouter()
@@ -48,11 +91,7 @@ const CreateGroup = () => {
           {available && (
             <>
               <h1>Create new {groupType ? groupType.title : 'group'}</h1>
-              <GroupForm
-                group={group}
-                type={groupType}
-                onSave={onSave}
-              />
+              <GroupForm group={group} type={groupType} onSave={onSave} />
             </>
           )}
           {!available && <LoadingPage />}
