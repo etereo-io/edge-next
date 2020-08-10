c81d754252b8a4dd8bddc38c2ba024f769d1ebf3
Ihor Anishchenko <igor.anishenko@kitrum.com>
2020-08-10 19:01:55 +0300

    feat(group summary view): add 'join' button

--- components/groups/read/group-summary-view/group-summary-view.tsx
+++ components/groups/read/group-summary-view/group-summary-view.tsx
@@ -1,62 +1,122 @@
-import DynamicFieldView from '@components/generic/dynamic-field/dynamic-field-view'
-import { FIELDS } from '@lib/constants'
-import GroupMembers from '@components/groups/group-members/group-members'
+import React, { memo, useCallback, useEffect, useState } from 'react'
 import Link from 'next/link'
 
-export default function(props) {
-  const shouldAddLink = (field) => {
-    return (
-      props.linkToDetail &&
-      field.type !== FIELDS.IMAGE &&
-      field.type !== FIELDS.FILE &&
-      field.type !== FIELDS.TAGS &&
-      field.type !== FIELDS.VIDEO_URL
-    )
-  }
+import { FIELDS } from '@lib/constants'
+import fetch from '@lib/fetcher'
+import { useUser } from '@lib/client/hooks'
+import { groupUserPermission } from '@lib/permissions'
+import API from '@lib/api/api-endpoints'
+import { GroupEntityType, GroupTypeDefinition } from '@lib/types'
+import DynamicFieldView from '@components/generic/dynamic-field/dynamic-field-view'
+import GroupMembers from '@components/groups/group-members/group-members'
+import Button from '@components/generic/button/button'
+
+interface Props {
+  linkToDetail: boolean
+  group: GroupEntityType
+  className: string
+  type: GroupTypeDefinition
+}
+
+function SummaryView({ linkToDetail, group, className, type }: Props) {
+  const [clicked, setClicked] = useState<boolean>(false)
+  const [error, setError] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [inMembersList, setInMembersList] = useState<boolean>(false)
+  const [inPendingMembersList, setInPendingMembersList] = useState<boolean>(
+    false
+  )
+  const shouldAddLink = useCallback(
+    (field) => {
+      return (
+        linkToDetail &&
+        ![FIELDS.IMAGE, FIELDS.FILE, FIELDS.TAGS, FIELDS.VIDEO_URL].includes(
+          field.type
+        )
+      )
+    },
+    [linkToDetail]
+  )
+
+  const currentUser = useUser()
+  const canJoin = groupUserPermission(
+    currentUser?.user,
+    type.slug,
+    'join',
+    group
+  )
+
+  useEffect(() => {
+    if (currentUser.user) {
+      const {
+        user: { id: currentUserId },
+      } = currentUser
+
+      setInMembersList(
+        (group.members || []).some(({ id }) => currentUserId === id)
+      )
+      setInPendingMembersList(
+        (group.pendingMembers || []).some(({ id }) => currentUserId === id)
+      )
+    }
+  }, [currentUser, group])
+
+  const handleJoinRequest = useCallback(() => {
+    setIsLoading(true)
+    fetch(`${API.groups[group.type]}/${group.slug}/users`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(currentUser.user),
+    })
+      .then(() => {
+        setClicked(true)
+        setError(false)
+      })
+      .catch(() => setError(true))
+      .finally(() => setIsLoading(false))
+  }, [currentUser, group.type, group.slug])
 
   return (
     <>
-      <div className={`group-summary-view ${props.className}`}>
+      <div className={`group-summary-view ${className}`}>
         <div className="">
           <div className="group-top-section">
-            {props.type.fields
-              .filter((f) => f.name === props.type.publishing.title)
+            {type.fields
+              .filter((f) => f.name === type.publishing.title)
               .map((field) => {
                 return (
                   <h1
                     className="content-title"
-                    key={`${field.name}-${props.group.id}`}
+                    key={`${field.name}-${group.id}`}
                   >
-                    {props.linkToDetail && (
-                      <Link
-                        href={`/group/${props.type.slug}/${props.group.slug}`}
-                      >
-                        <a>{props.group[field.name]}</a>
+                    {linkToDetail && (
+                      <Link href={`/group/${type.slug}/${group.slug}`}>
+                        <a>{group[field.name]}</a>
                       </Link>
                     )}
-                    {!props.linkToDetail && props.group[field.name]}
+                    {!linkToDetail && group[field.name]}
                   </h1>
                 )
               })}
 
-            { props.group.members && <GroupMembers members={props.group.members} visible={3} />}
+            {group.members && (
+              <GroupMembers members={group.members} visible={3} />
+            )}
           </div>
 
-          {props.type.fields
+          {type.fields
             .filter((f) => !f.hidden)
-            .filter((f) => f.name !== props.type.publishing.title)
+            .filter((f) => f.name !== type.publishing.title)
             .map((field) => {
               return (
-                <div key={`${field.name}-${props.group.id}`}>
+                <div key={`${field.name}-${group.id}`}>
                   {shouldAddLink(field) && (
-                    <Link
-                      href={`/group/${props.type.slug}/${props.group.slug}`}
-                    >
+                    <Link href={`/group/${type.slug}/${group.slug}`}>
                       <a title="Go to item detail">
                         <DynamicFieldView
                           field={field}
-                          value={props.group[field.name]}
-                          typeDefinition={props.type}
+                          value={group[field.name]}
+                          typeDefinition={type}
                         />
                       </a>
                     </Link>
@@ -64,13 +124,26 @@ export default function(props) {
                   {!shouldAddLink(field) && (
                     <DynamicFieldView
                       field={field}
-                      value={props.group[field.name]}
-                      typeDefinition={props.type}
+                      value={group[field.name]}
+                      typeDefinition={type}
                     />
                   )}
                 </div>
               )
             })}
+          <div className="join-to-group-section">
+            {(inPendingMembersList || clicked) && (
+              <Button warning restProps={{ disabled: true }}>
+                Request to join is in Pending status
+              </Button>
+            )}
+            {canJoin && !inPendingMembersList && !inMembersList && !clicked && (
+              <Button success loading={isLoading} onClick={handleJoinRequest}>
+                Join
+              </Button>
+            )}
+            {error && <div>Something went wrong.</div>}
+          </div>
         </div>
       </div>
       <style jsx>{`
@@ -101,8 +174,12 @@ export default function(props) {
           color: var(--edge-link-color);
         }
 
-        
+        .join-to-group-section {
+          text-align: right;
+        }
       `}</style>
     </>
   )
 }
+
+export default memo(SummaryView)
