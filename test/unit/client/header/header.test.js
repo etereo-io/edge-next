e0962a7454b76241d5997c1c504705fa64c10b9b
rafinskipg <rafinskipg@gmail.com>
2021-01-07 17:12:26 +0100

    fix tests

--- test/unit/client/header/header.test.js
+++ test/unit/client/header/header.test.js
@@ -4,6 +4,20 @@ import React from 'react'
 import { render } from '@testing-library/react'
 
 describe('header test suite', () => {
+  beforeAll(() => {
+    const observe = jest.fn();
+    const unobserve = jest.fn();
+    const disconnect = jest.fn();
+
+    // you can also pass the mock implementation
+    // to jest.fn as an argument
+    window.IntersectionObserver = jest.fn(() => ({
+      observe,
+      unobserve,
+      disconnect
+    }))
+  })
+
   test('renders login link in header', () => {
     const { getByText } = render(
       <EdgeUserProvider>
--- test/unit/client/index.test.js
+++ test/unit/client/index.test.js
@@ -3,15 +3,30 @@ import Index from '../../../pages/index'
 import React from 'react'
 import { render } from '@testing-library/react'
 
-test('renders login link', () => {
-  const { getAllByText } = render(
-    <EdgeUserProvider>
-      <Index />
-    </EdgeUserProvider>
-  )
-  const linkElements = getAllByText(/Login/)
+describe('Home page', () => {
+  beforeAll(() => {
+    const observe = jest.fn();
+    const unobserve = jest.fn();
+    const disconnect = jest.fn();
 
-  linkElements.map((i) => {
-    expect(i).toBeInTheDocument()
+    // you can also pass the mock implementation
+    // to jest.fn as an argument
+    window.IntersectionObserver = jest.fn(() => ({
+      observe,
+      unobserve,
+      disconnect
+    }))
+  })
+  test('renders login link', () => {
+    const { getAllByText } = render(
+      <EdgeUserProvider>
+        <Index />
+      </EdgeUserProvider>
+    )
+    const linkElements = getAllByText(/Login/)
+  
+    linkElements.map((i) => {
+      expect(i).toBeInTheDocument()
+    })
   })
 })
